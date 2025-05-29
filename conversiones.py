0da7ab09d42a6a45b031539d83c52043381369d5
DavidPToyo <davidpichardot31@hotmail.com>
2025-05-29 21:30:01 +0000

    Listo

--- conversiones.py
+++ conversiones.py
@@ -1,18 +1,26 @@
 import sys
 
+#Introducir valores:
+
+# Sol peruano = 0.0046
+# Peso Argentino = 0.093
+# Dolar americano = 0.0011
+
+
 if len(sys.argv) != 5:
     print("python conversiones.py 'Sol Peruano' 'Pesos Argentino' 'Dolares Americano' 'Pesos Chilenos' ")
     sys.exit(1)
 
 
 conversiones = {
-    "Soles_Peruanos" : (float(sys.argv[1]) * float(sys.argv[-1])),
-    "Pesos_Argentino" : (float(sys.argv[2]) * float(sys.argv[-1])),
-    "Dolares_Americanos" : (float(sys.argv[3]) * float(sys.argv[-1]))
+    "Soles Peruanos" : (float(sys.argv[1]) * float(sys.argv[-1])),
+    "Pesos Argentino" : (float(sys.argv[2]) * float(sys.argv[-1])),
+    "Dolares Americanos" : (float(sys.argv[3]) * float(sys.argv[-1]))
 }
+
+print(f"Los {sys.argv[-1]} pesos chilenos equivalen a:")
 
 for k, v in conversiones.items():
 
-    print(f"{sys.argv[-1]} pesos chilenos equivalen a:")
     print(k, v)
     
